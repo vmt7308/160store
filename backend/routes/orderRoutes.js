cb604895f8282dc3c62954b994c641e64e67080b
vmt7308 <vmt7308@gmail.com>
2025-12-15 18:18:11 +0700

    Khach hang co the huy don hang truoc khi he thong xac nhan

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   createOrder,
   getOrdersByUser,
+  cancelOrder,
 } = require("../controllers/orderController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware, createOrder);
 router.get("/user/:userId", authMiddleware, getOrdersByUser);
+router.post("/cancel/:orderId", authMiddleware, cancelOrder);
 
 module.exports = router;
