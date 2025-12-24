61a4a9e0856ea5526f7fe53843655adbf0ce8d35
vmt7308 <vmt7308@gmail.com>
2025-12-24 18:53:15 +0700

    Fix luon luon hien thi o dau trang

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -15,9 +16,20 @@ import SearchResults from "./pages/SearchResults";
 
 import Admin from "./pages/Admin";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
